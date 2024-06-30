48f495148d3fdf956d6ab98107d5525e506a206f
Ainsley <ainsleyjyl@gmail.com>
2024-06-29 20:37:24 -0700

    create rootlayout

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,9 +1,6 @@
-import Navigation from '../components/Navigation';
-
 export default function page() {
     return (
         <div>
-            <Navigation />
             <h1 className=''>Cabins page</h1>;
         </div>
     );
--- app/page.js
+++ app/page.js
@@ -1,10 +1,6 @@
-import Link from 'next/link';
-import Navigation from './components/Navigation';
-
 export default function Home() {
     return (
         <div>
-            <Navigation />
             <h1>Welcome to Wild Oasis</h1>
         </div>
     );

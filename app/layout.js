02ea81b703f712cb622777bf7cb3f1dbc7459055
Ainsley <ainsleyjyl@gmail.com>
2024-07-13 20:16:26 -0700

    install incons

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang='en'>
-            <body className='bg-blue-900'>
+            <body className='bg-primary-900 text-primary-100 min-h-screen'>
                 <header>
                     <Logo />
                     <Navigation />

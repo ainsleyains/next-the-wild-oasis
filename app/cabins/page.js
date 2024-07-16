c35d3840a34c23f8ce68271aa2466e303176a43c
Ainsley <ainsleyjyl@gmail.com>
2024-07-15 21:31:06 -0700

    add about page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,8 +1,11 @@
+import CabinCard from '@/app/_components/CabinCard';
+
 export const metadata = {
     title: 'Cabins',
 };
 
 export default function Cabins() {
+    const cabins = [];
     return (
         <div>
             <h1 className='text-4xl mb-5 text-accent-400 font-medium'>Our Luxury Cabins</h1>
@@ -12,6 +15,13 @@ export default function Cabins() {
                 private hot tub under the stars. Enjoy nature&apos;s beauty in your own little home away from home. The
                 perfect spot for a peaceful, calm vacation. Welcome to paradise.
             </p>
+
+            {cabins.map((cabin) => (
+                <CabinCard
+                    cabin={cabin}
+                    key={cabin.id}
+                />
+            ))}
         </div>
     );
 }

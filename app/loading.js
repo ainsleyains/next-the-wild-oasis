dd5052c3ca3473868a1c765746444080585e6595
Ainsley <ainsleyjyl@gmail.com>
2024-07-21 12:41:37 -0700

    add loading to cabin page

--- app/loading.js
+++ app/loading.js
@@ -1,7 +1,5 @@
+import Spinner from '@/app/_components/Spinner';
+
 export default function loading() {
-    return (
-        <div className=''>
-            <p>Loading...</p>
-        </div>
-    );
+    return <Spinner />;
 }
